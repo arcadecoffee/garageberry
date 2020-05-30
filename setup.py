f275006dcd56aa39eb259345fd7fbb03e4e0df75
Jerry Gregoire <jerry@arcadecoffee.com>
2020-05-30 11:04:42 -0500

    .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='garageberry',
+    version='',
+    packages=['garageberry'],
+    url='',
+    license='',
+    author='Jerry Gregoire',
+    author_email='',
+    description=''
+)
