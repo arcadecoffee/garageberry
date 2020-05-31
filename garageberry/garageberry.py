66dc89d7eb37e2a5d7e69707b2d36f085a62befc
Jerry Gregoire <jerry@arcadecoffee.com>
2020-05-31 15:47:34 -0500

    .

--- garageberry/garageberry.py
+++ garageberry/garageberry.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from gpiozero import LED, InputDevice
 
-app = FastAPI()
+try:
+    from gpiozero.pins.rpigpio import RPiGPIOFactory as DefaultPinFactory
+except ModuleNotFoundError:
+    from gpiozero.pins.mock import MockFactory as DefaultPinFactory
 
+class GarageDoor(object):
+    def __init__(self, sensor_pin=None, switch_pin=None, light_pin=None, pin_factory=DefaultPinFactory()):
+        self._pin_factory = pin_factory
+        self.sensor = InputDevice(pin=sensor_pin, pin_factory=pin_factory)
+        self.door = LED(pin=switch_pin, pin_factory=pin_factory)
+        self.light = LED(pin=light_pin, pin_factory=pin_factory)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    @property
+    def door_status(self):
+        return 'closed' if self.is_closed else 'open'
 
+    @property
+    def is_closed(self):
+        return self.sensor.is_active
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    def toggle_door(self):
+        self.door.blink(on_time=0.5, n=1)
+
+    def close_door(self):
+        if not self.is_closed:
+            self.toggle_door()
+
+    def open_door(self):
+        if self.is_closed:
+            self.toggle_door()
+
+    def toggle_light(self):
+        self.light.blink(on_time=0.5, n=1)
